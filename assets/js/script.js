fe2452b8ac6bfc85d0d10d24e08aad604c91224d
wlillisdev <williamlillis100@gmail.com>
2022-01-17 20:37:28 +0000

    new gif inserted

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ function check() {
 
     // define varabile and the array of pictuers types and comments that will appear .
     //calcuated in relation to there final score.
-    var pictures = ["assets/images/Excellent.gif", "assets/images/smart.gif", "assets/images/bad-score.gif"];
+    var pictures = ["assets/images/Excellent-1.gif", "assets/images/smart.gif", "assets/images/bad-score.gif"];
     var messages = ["Amazing! You Rock", "Good Job,Smart but not a Genius", "Not Great,Try Again"];
     
     //determines 3 diffrent ranges for users score .
