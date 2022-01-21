a58dacf208abc618e80f54c6b514e4da50b6be14
wlillisdev <williamlillis100@gmail.com>
2022-01-21 20:09:52 +0000

    added quizzes java script page

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,11 +31,11 @@ function checkQuizz() {
 
     // var elements = document.querySelectorAll("#quiz-container input[type=radio][value='']");
     // for (var i = 0, element;  element = elements [i++]; {
-        
+
     //     if (_element.value === '')
     //     alert ('You missed question');
     //     return false;
-           
+
     // })
 
 
@@ -84,6 +84,21 @@ function checkQuizz() {
     }
     //Top Marks all correct.
 
+    var item = document.getElementById("quiz-container");
+    var yes = document.getElementById("yes");
+    var no = document.getElementById("no");
+
+    yes.onclick = function () {
+        item.style.backgroundColor = "red";
+    };
+
+    no.onclick = function () {
+        item.style.backgroundColor = "green";
+    };
+
+
+    
+
     //after user submits quizz this section will appear.
     document.getElementById("after_submit").style.visibility = "visible";
     //after user submits quizz user final score,a message and a pictuer will appear.
@@ -92,6 +107,7 @@ function checkQuizz() {
     document.getElementById("picture").src = pictures[range];
 
 }
+
 
 
 //Animates the text on the hero on the home page, adapted to site -credit https://codepen.io/bulldogpatch-the-sasster/pen/JjoyeGg.
@@ -130,32 +146,31 @@ function complete() {
 // Define Varaibles and elements for fact generator
 let btn = document.getElementById("btn");
 let output = document.getElementById("fact");
-let facts = 
-// Create an array an of random facts
-[
-    'Tic Tac mints are named after the sound their container makes.',
-    'The largest volcano in the solar system is three times taller than Mount Everest.',
-    'The world record for the tallest stack of doughnuts totaled more than 3,000.',
-    'The fastest gust of wind ever recorded on Earth was 253 miles per hour.',
-    'The Canary Islands are named after dogs, not birds.',
-    'Four babies are born every second.',
-    'There are around 4 quadrillion quadrillion bacteria on Earth.',
-    'There are 43 countries that still have a royal family.',
-    'All giant pandas in zoos around the world are on loan from China.',
-    'Bats are the only mammal that can actually fly.',
-    'Like humans, koalas actually have unique individual fingerprints',
-    'Australia is wider than the moon.',
-    'Humans are the only animals on earth with chins.',
-    'More than 480million people have played Monopoly.',
-    'A bolt of lightning is five times hotter than the sun.',
-    'Dogs hearing is 10 times better than a humans.',
-    'There are more stars in space than there are grains of sand on a beach.',
-    'A shark is the only known fish that can blink with both eyes',
+let facts =
+    // Create an array an of random facts
+    [
+        'Tic Tac mints are named after the sound their container makes.',
+        'The largest volcano in the solar system is three times taller than Mount Everest.',
+        'The world record for the tallest stack of doughnuts totaled more than 3,000.',
+        'The fastest gust of wind ever recorded on Earth was 253 miles per hour.',
+        'The Canary Islands are named after dogs, not birds.',
+        'Four babies are born every second.',
+        'There are around 4 quadrillion quadrillion bacteria on Earth.',
+        'There are 43 countries that still have a royal family.',
+        'All giant pandas in zoos around the world are on loan from China.',
+        'Bats are the only mammal that can actually fly.',
+        'Like humans, koalas actually have unique individual fingerprints',
+        'Australia is wider than the moon.',
+        'Humans are the only animals on earth with chins.',
+        'More than 480million people have played Monopoly.',
+        'A bolt of lightning is five times hotter than the sun.',
+        'Dogs hearing is 10 times better than a humans.',
+        'There are more stars in space than there are grains of sand on a beach.',
+        'A shark is the only known fish that can blink with both eyes',
 
-];
+    ];
 // Add an event lister with a function when clicked that will generate randon fact.
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     var randomFacts = facts[Math.floor(Math.random() * facts.length)];
     output.innerHTML = randomFacts;
 });
-
