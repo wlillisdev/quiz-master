fffb200f1e19530220171b937b8b76cdb50902ab
wlillisdev <williamlillis100@gmail.com>
2022-01-23 18:43:25 +0000

    add property function

--- assets/js/quizzes.js
+++ assets/js/quizzes.js
@@ -10,24 +10,37 @@ function initMap() {
     zoom: 3,
     center: dublin,
   });
-  // // The marker, positioned at Dublin
-  // let marker = new google.maps.Marker({
-  //   position: dublin,
-  //   map: map,
-  // });
+  // The marker, positioned at Dublin
+  let marker = new google.maps.Marker({
+    position: dublin,
+    map: map,
+  });
 
-  // //InfoWindow
-  // const detailWindow = new google.maps.InfoWindow({
-  //   content: `<h3>Dublin</h3><p>Did you know that</p>`
-  // });
+  //InfoWindow
+  const detailWindow = new google.maps.InfoWindow({
+    content: `<h3>Dublin</h3><p>Did you know that</p>`
+  });
 
-  // marker.addListener("mouseover", () => {
-  //   detailWindow.open(map, marker);
-  // })
+  marker.addListener("mouseover", () => {
+    detailWindow.open(map, marker);
+  })
 
-  
 
-  
+  // Add Marker
+  function addMarker(property) {
+
+    const marker = new google.maps.Marker({
+      position: property.location,
+      map: map,
+      //icon: property.imageIcon
+    });
+
+
+  }
+
+
+
+
 
 
 
