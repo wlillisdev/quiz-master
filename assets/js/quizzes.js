7cc0bc9299fb557493c793be26aae14586110f6a
wlillisdev <williamlillis100@gmail.com>
2022-01-23 18:30:28 +0000

    google map set up on quizes page

--- assets/js/quizzes.js
+++ assets/js/quizzes.js
@@ -1,8 +1,37 @@
-let map;
-
+// Initialize and add the map
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 53.3498, lng: -6.2603 },
-    zoom: 2,
+  // The location of Dublin
+  const dublin = {
+    lat: 53.3498,
+    lng: -6.2603
+  };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 3,
+    center: dublin,
   });
+  // // The marker, positioned at Dublin
+  // let marker = new google.maps.Marker({
+  //   position: dublin,
+  //   map: map,
+  // });
+
+  // //InfoWindow
+  // const detailWindow = new google.maps.InfoWindow({
+  //   content: `<h3>Dublin</h3><p>Did you know that</p>`
+  // });
+
+  // marker.addListener("mouseover", () => {
+  //   detailWindow.open(map, marker);
+  // })
+
+  
+
+  
+
+
+
+
+
+
 }
