6cd24a89cfc8f74100e88911b2e9d081976ab85f
wlillisdev <williamlillis100@gmail.com>
2022-01-25 19:31:38 +0000

    add a loop and array to add marker function in quizzes java script

--- assets/js/quizzes.js
+++ assets/js/quizzes.js
@@ -1,25 +1,15 @@
 // Initialize and add the map
 function initMap() {
-  // // The location of Dublin
-  // const dublin = {
-  //   lat: 53.3498,
-  //   lng: -6.2603
-  // };
-
   let options = {
     center: {
       lat: 53.3498,
       lng: -6.2603
     },
-    zoom: 2
+    zoom: 4
   }
   // The new map, centered at Dublin
   map = new google.maps.Map(document.getElementById("map"), options)
-  // // The marker, positioned at Dublin
-  // let marker = new google.maps.Marker({
-  //   position: dublin,
-  //   map: map,
-  // });
+
 
   // //InfoWindow
   // const detailWindow = new google.maps.InfoWindow({
@@ -54,6 +44,45 @@ function initMap() {
   //   detailWindow.open(map, marker);
   // })
 
+  //Add Markers and content to an Array
+
+  let MarkerArray = [
+
+    {
+      location: {
+        lat: 37.9922,
+        lng: -1.1307
+      },
+      content: `<h2>Murcia City</h2><p>did you know that the</p>`
+    }
+
+    {
+      location: {
+        lat: 39.4699,
+        lng: -0.3763,
+      },
+      content: `<h2>Murcia City</h2><p>did you know that the</p>`
+    }
+
+
+
+
+
+
+  
+  
+  ]
+
+
+
+  for (let i = 0; i < MarkerArray.length; i++){
+    addMarker(MarkerArray[i])
+  }
+
+
+
+
+
   function addMarker(property) {
 
     const marker = new google.maps.Marker({
@@ -74,21 +103,21 @@ function initMap() {
   }
 
 
-  addMarker({
-    location: {
-      lat: 37.9922,
-      lng: -1.1307
-    },
-    content: `<h2>Murcia City</h2><p>did you know that the</p>`
-  });
+  // addMarker({
+  //   location: {
+  //     lat: 37.9922,
+  //     lng: -1.1307
+  //   },
+  //   content: `<h2>Murcia City</h2><p>did you know that the</p>`
+  // });
 
-  addMarker({
-    location: {
-      lat: 39.4699,
-      lng: -0.3763,
-    },
-    content: `<h2>Murcia City</h2><p>did you know that the</p>`
-  });
+  // addMarker({
+  //   location: {
+  //     lat: 39.4699,
+  //     lng: -0.3763,
+  //   },
+  //   content: `<h2>Murcia City</h2><p>did you know that the</p>`
+  // });
 
 
 
